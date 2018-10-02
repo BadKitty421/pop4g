f99d7fa662e712f37669e8324e856d65913e1983
BadKitty421 <43781398+BadKitty421@users.noreply.github.com>
2018-10-02 15:17:13 +0200

    Update 4g.0.fsx

--- 4g.0.fsx
+++ 4g.0.fsx
@@ -2,6 +2,10 @@ let len (x:float, y:float) : float =
   let v = sqrt(x**2.0 + y**2.0)
   v
 
+let ang (x:float, y:float) : float = 
+  let v = atan2 x y
+  v
+
 
 let scale (a:float) (x:float, y:float) : float*float =
   let x2 = a*x
@@ -22,6 +26,7 @@ let dot (x1:float, y1:float) (x2:float, y2:float) : float =
   v 
 
 printfn "%A" (len (2.0, 2.0))
+printfn "%A" (ang (2.0, 3.0))
 printfn "%A" (scale 2.0 (2.0, 2.0))
 printfn "%A" (add (2.0, 1.0)(1.0, 2.0))
 printfn "%A" (dot (1.0, 2.0) (3.0, 1.0))
