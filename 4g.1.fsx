ee79926b3205d0c96a33237ad3b18a510779b456
BadKitty421 <43781398+BadKitty421@users.noreply.github.com>
2018-10-02 15:35:56 +0200

    Add files via upload

--- 4g.1.fsx
+++ 4g.1.fsx
@@ -6,13 +6,13 @@ printfn "      Branch: 0 - %b" (len (3.0,4.0) = 5.0)
 
 
 printfn "   Unit: Ang"
-printfn "      Branch: 0 - %b" ()
+printfn "      Branch: 0 - %b" ((ang (1.0, 0.0)) = 0.0)
 
 printfn "   Unit: Scale"
-printfn "      Branch: 0 - %b" ((scale 3.0 (2.0, 3.0) = (3.0,9.0))
+printfn "      Branch: 0 - %b" ((scale 3.0 (2.0, 3.0)) = (3.0,9.0))
 
 printfn "   Unit: Add"
-printfn "      Branch: 0 - %b" ((add (2.0, 3.0) (4.0, 5.0)) = (6.0, 8.0)
+printfn "      Branch: 0 - %b" (((add (2.0, 3.0) (4.0, 5.0)) = (6.0, 8.0))
 
 printfn "   Unit: Dot"
 printfn "      Branch: 0 - %b" ((dot (2.0, 3.0) (4.0, 5.0)) = 23))
