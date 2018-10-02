8cfc762b6a44d1c9e795958d224d44039e0d963b
BadKitty421 <43781398+BadKitty421@users.noreply.github.com>
2018-10-02 13:47:42 +0200

    Add files via upload

---
+++ 4g.0.fsx
@@ -0,0 +1,27 @@
+let len (x:float, y:float) : float =
+  let v = sqrt(x**2.0 + y**2.0)
+  v
+
+
+let scale (a:float) (x:float, y:float) : float*float =
+  let x2 = a*x
+  let y2 = a*y
+  let v = (x2, y2)
+  v
+
+let add (x1:float, y1:float) (x2:float, y2:float) : float*float =
+  let x3 = x1 + x2
+  let y3 = y1 + y2
+  let v = (x3, y3)
+  v
+
+let dot (x1:float, y1:float) (x2:float, y2:float) : float =
+  let x3 = x1 * x2
+  let y3 = y1 * y2
+  let v = x3 + y3
+  v 
+
+printfn "%A" (len (2.0, 2.0))
+printfn "%A" (scale 2.0 (2.0, 2.0))
+printfn "%A" (add (2.0, 1.0)(1.0, 2.0))
+printfn "%A" (dot (1.0, 2.0) (3.0, 1.0))
